729498c5e9e91a704787a94df138d4e91ee9a522
Armand Christian <armandchristian85@gmail.com>
2026-09-04 21:38:56 +0100

    prepared Frontend for deployment

---
+++ frontend/.output/public/assets/erp-logo-CkwGkpNV.js
@@ -0,0 +1,1 @@
+const o="/assets/erp-logo-yeQXjU4Z.png";export{o as l};
---
+++ frontend/.output/public/assets/notifications.service-lEX-8nnW.js
@@ -0,0 +1,1 @@
+import{o}from"./index-DlqZle1Y.js";const a=t=>o.get("/notifications",{params:t}),s=t=>o.patch(`/notifications/${t}/lire`),e=()=>o.patch("/notifications/tout-lire");export{e as a,a as g,s as m};
